1441cac46e1d2fe91f1ea1e939a9faf8f6156a83
Sapphiron532 <xxl3d3@gmail.com>
2025-06-16 18:39:58 +0300

    удаление неиспользуемых компонентов

--- src/components/viewer/index.ts
+++ src/components/viewer/index.ts
@@ -1,11 +1,7 @@
 export { default as SlideViewer } from './SlideViewer/SlideViewer';
 export { default as PresentationViewer } from './PresentationViewer';
-export { default as FullscreenViewer } from './FullscreenViewer';
 export { default as LayoutViewer } from './LayoutViewer';
 export { default as ElementViewer } from './ElementViewer';
 export { default as ViewerTemplateImage } from './ViewerTemplateImage';
-export { default as ThemedPresentationViewer } from './ThemedPresentationViewer';
-export { default as ThemedFullscreenViewer } from './ThemedFullscreenViewer';
 export { default as ThemeStylesApplier } from './theme/ThemeStylesApplier';
 export { default as ViewerProvider } from './ViewerProvider';
-export { default as PresentationPreview } from './PresentationPreview';
