f062e9ff15b6935a37145be3338abe321b69d127
Sapphiron532 <xxl3d3@gmail.com>
2025-06-20 19:38:25 +0300

    Revert "скрытие настройки маски изображения"
    This reverts commit 557faf1e245ba5dace02df4e421818b1305c04ef.

--- src/components/theme/components/CardsDesign/CardsDesign.tsx
+++ src/components/theme/components/CardsDesign/CardsDesign.tsx
@@ -166,8 +166,7 @@ export default function CardsDesign({
             </div>
 
             {/* Image Shape */}
-            {/* FIXME: нужно поправить применение при экспорте в pdf */}
-            {/* <div>
+            <div>
                 <Label>Форма изображения</Label>
                 <div style={{ display: 'flex', marginTop: '0.5rem', gap: '0.5rem', flexWrap: 'wrap' }}>
                     {['default', 'fade', 'diagonal', 'round', 'round-inverse', 'wiggle'].map(option => (
@@ -199,7 +198,7 @@ export default function CardsDesign({
                         </button>
                     ))}
                 </div>
-            </div> */}
+            </div>
         </div>
     );
 }
